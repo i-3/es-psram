3f834d66a3dfce3d0af9e032f85aacbdb8e6fa29
i-3 <freimann.dev@gmail.com>
2026-07-22 21:47:30 +0300

    edit log output

--- main/es-psram.c
+++ main/es-psram.c
@@ -6,16 +6,15 @@ static const char *TAG = "MAIN";
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "====================================");
-    ESP_LOGI(TAG, "   Проект es-psram успешно запущен!  ");
-    ESP_LOGI(TAG, "====================================");
+    ESP_LOGI(TAG, "=================================");
+    ESP_LOGI(TAG, " PSRAM initialized successfully! ");
+    ESP_LOGI(TAG, "=================================");
 
-    // Проверяем, инициализирована ли PSRAM в sdkconfig
     if (esp_psram_is_initialized()) {
         size_t psram_size = esp_psram_get_size();
-        ESP_LOGI(TAG, "PSRAM обнаружена! Размер: %d МБ (%d байт)", 
-                 psram_size / (1024 * 1024), psram_size);
+        ESP_LOGI(TAG, "PSRAM detected! Size: %d MB (%zu bytes)", 
+                 (int)(psram_size / (1024 * 1024)), psram_size);
     } else {
-        ESP_LOGW(TAG, "PSRAM не инициализирована (или выключена в menuconfig)!");
+        ESP_LOGW(TAG, "PSRAM is not initialized (or disabled in menuconfig)!");
     }
 }
